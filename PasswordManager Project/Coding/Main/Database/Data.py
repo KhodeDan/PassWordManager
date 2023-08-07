9aae64ccf914be62c0718b2a038be7fb696dd8b2
KhodeDan <khodedancollab@gmail.com>
2023-08-07 10:18:08 +0330

    PaswordManager | V : 1.4.15
    Architecture And Structure :
    
           Built And added  A new Structured Algorithm For Collect_Birthdate()
    
    
    Filing And Database:
    
          Changed the filing Structure : Algorithm's and Information text's Are now In different Filing In the same directory They were.

--- PasswordManager Project/Coding/Main/Database/Data.py
+++ PasswordManager Project/Coding/Main/Database/Data.py
@@ -1,34 +1,18 @@
+from dataclasses import dataclass
 
-
-"_____________________ Imports _______________________"
-
-
-from time import sleep 
-
+from time import sleep
 
 import os
 
-
-# Used In exiting Oepration.
 import sys
-
-
-"______________________________________________________"
-
-
-"__________________ Needed Variables ________________"
 
 
 version = F'Beta 1.4.13'     # *Semantic Numbering.
 
-
-# Define a empty dictionary , Which Later Is going to contain Username's , Masterpassword's , Password's.
 Users_Info = {
     "Users" : {}
 }
 
-
-# A dictionary of all the avaliable Operation's that can be used in the main menu , All assigned with a number.
 Operations = {  1 : "Viewing Passwords"
               , 2 : "Adding Password" 
               , 3 : "Removing Password" 
@@ -36,22 +20,18 @@ Operations = {  1 : "Viewing Passwords"
               , 5 : "ForGot MasterPassword"
               , 6 : "Quit"}
 
-
 user_management_operations = {
       7 : "Change User"
     , 8 : "Add User" 
     , 9 : "Delete user"
 }
 
-
-# Define all the letter's possible to write In a password , Uppercase and Lowercase letter's and number's for further use.
+# Usage in different Input accepting , Denying.
 allowed = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ,"0" , "1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , ]
 UpperCase_Letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ,]
 LowerCase_Letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 Numbers = ["1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , "0"]
 
-
-# All Case situation's of the word Quit , In order to be used in the quitting situation's
 QUIT_LETTERCASE = [
     "QUIT",
     "Quit",
@@ -76,9 +56,7 @@ QUIT_LETTERCASE = [
     "quIt"
 ]
 
-
 TABS = "\t"
-
 
 # ANSI COLORING.
 color_red = "\33[31m"
@@ -91,26 +69,13 @@ format_reset = "\33[0m"
 indent = print("")
 
 
-"_____________________________ Clearing Function _______________________________"
-
-
-# Shorted function for os.system('cls')
 def clean() -> None:
 
-
     os.system('cls')
-
 
 clean()
 
 
-"_______________________________________________________________________________"
-
-
-"______________________________ Loading Look-Function __________________________"
-
-
-# Define a function called loading_Look , Which Have the order of starting the main loading look at the start of the code.
 def loading():
     print(F"{color_blue}Loading{color_red} Assets....")
     sleep(1)
@@ -140,67 +105,33 @@ def loading():
     clean()
 
 
-"__________________________________________________________________________"
-
-
-"_____________________________ Termination (Exiting) Operation __________________________"
-
-
-# Define a function called termination , Which Have the part of cleaning , Exiting The program In an ordinary way .
 def termination():
 
-
             clean()
-
-
             print("\33[Thanks for Using KhodeDan's Password Manager.")
-
-
             sleep(4)
-
-
             clean()
-
-
             print("\33[1;31;40mQuitting.... \33[0m")
-
-
             sleep(3)
-
-
             clean()
-
 
             sys.exit()   # This syntax used with sys module , Will tell the python interpreter to terminate the project.
-
-
-"_______________________________ Termination (Exiting) Operation ___________________________"
-
-
-"_________________________ UserName Checking Function______________________"
 
 
 # Define a function that will check the Chosen UserName , And see If it meet's the minimum Requirement's.
 def isusername(UserName : str):
 
-
-    UserName_Last_Limit = 13     # Define a variable with the value of 13 , Which Later will be used for the lengh checking of the UserName.
-    UserName_First_limit = 3     # Define A variable with the value of 13 , Which Later will be used for the lengh chdcking of the Username.
-    UserName_Lengh = len(UserName)   # Check the lengh of the username and store It in a variable.
-
+    UserName_Last_Limit = 13     
+    UserName_First_limit = 3     
+    UserName_Lengh = len(UserName)   
 
 
     if UserName_First_limit > UserName_Lengh :
         return False
 
     
-    
     if UserName_Last_Limit < UserName_Lengh:
         return False
-    
-
-
-
     
 
     if UserName.isdigit():
@@ -209,32 +140,18 @@ def isusername(UserName : str):
     
     for character in UserName:
 
-
         if character not in allowed:
+        
             return False
     
-
     return True
 
 
 
-
-"_________________________ UserName Checking Function______________________"
-
-
-"_______________________ BirthYear Checking Function _______________________"
-
-
-# Define a function in order to make sure the user entered birthyear Is correct and logical.  
 def isbirthyear(birthyear : int):
     
-    # Define a variable with the value of 1923 , Which will act as the lowest birthyear that can be entered.
     Age_First_Limit = 1923
-    
-    # Define variable with the value of 2022 , Which means the Latest birthyear possible to use this program
     Age_Last_Limit = 2022
-
-
 
 
     try:
@@ -248,7 +165,6 @@ def isbirthyear(birthyear : int):
 
         return True
     
-
     except:
         return False
 
@@ -256,66 +172,45 @@ def isbirthyear(birthyear : int):
 "_______________________ Birthyear Checking Function ______________________"
 
 
-"_______________________ BirthMonth Checking Function ______________________"
 
 
-# Define a function called Is birthmonth in order to make sure a integer has all the needed option's to be counted as a birthmonth.
+
+
 def isbirthmonth(birthmonth : int):
 
-    try:
-    
-        birthmonth = birthmonth + 1
-        birthmonth_First_Limit = 1
-        birthmonth_Last_Limit = 13
+    birthmonth_First_Limit = 1
+    birthmonth_Last_Limit = 13
 
 
-
-
-        if birthmonth > birthmonth_Last_Limit :
-            return False
-        
-        
-        if birthmonth < birthmonth_First_Limit:
-            return False
-
-
-        return True
-    
-    
-    except:
+    if birthmonth > birthmonth_Last_Limit:
         return False
+    
+    if birthmonth < birthmonth_First_Limit:
+        return False
+    
 
-
-"_________________________ BirthMonth Checking Function ___________________"
-
-
-"________________________ BirthDay Checking Function ______________________"
+    return True
 
 
 def isbirthday(birthday : int):
 
-
     try:
-
 
         Birthday_First_limit = 1
         Birthday_Last_limit = 31
 
-        
+
         if birthday > Birthday_Last_limit:
             return False
         
-
         if birthday < Birthday_First_limit :
             return False
         
 
         return True
     
-
     except:
         
-
         return False
 
 
@@ -328,7 +223,6 @@ def isbirthday(birthday : int):
 # define a function called MasterPassword with a string argument in order to make sure a string Has all the option's needed to be counted as a masterPassword.
 def ismasterpassword(MasterPassword : str):
 
-
     point = 3
     MasterPasswordFirstLimit = 5
     MasterPasswordLastLimit = 15
@@ -340,16 +234,13 @@ def ismasterpassword(MasterPassword : str):
 
     for character in MasterPassword:
         
-
         if character in Numbers:
             HasNumber = True
             point = point - 1
 
-        
         elif character in UpperCase_Letters:
             HasUpperCase = True
             point = point - 1
-
         
         elif character in LowerCase_Letters:
             HasLowerCase = True
@@ -372,11 +263,11 @@ def ismasterpassword(MasterPassword : str):
 
 # Define a function called ispassword , Which will check if the user entry Is a real Password , In this Case , There Isn't much prohibited Case's , Because user may enter different type of Password's (Digit , ETC)
 def ispassword(password):
-    "------------------ Needed Variables --------------------"
+
     Password_Firstlimi = 1   # Define a variable with the value of int 1 , Which will act as the limitation for the user entry lengh.
     Password_Lastlimit = 30  # Define a variable with the value of int 50 , Which will act as the limitation for the user entry lengh.
     Password_Lengh = len(password)   # Define a variable which will keep the lengh of the entered password , As value.
-    "--------------------------------------------------------"
+
 
 
     if Password_Lengh > Password_Lastlimit:
@@ -388,12 +279,6 @@ def ispassword(password):
     
 
     return True
-
-
-
-
-
-"___________________________ MasterPassword Check Function ________________________"
 
 
 def equality_check(string : str , MasterPassword : str) -> bool:
@@ -425,3 +310,5 @@ def get_key_by_value(dictionary : dict , value):
         
 
         return None
+
+    
